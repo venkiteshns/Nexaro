edde77b440a7aa41346b1e4bf91bbaa78ad62445
venkiteshns <software.venkitesh.in@gmail.com>
2026-08-29 08:36:32 +0530

    removed cloudinary import in uploadUtils.js

--- back-end/src/utils/uploadUtils.js
+++ back-end/src/utils/uploadUtils.js
@@ -1,55 +1,8 @@
-import cloudinary from "../config/cloudinary.js";
 import path from "path";
 import fs from "fs"
 import { randomUUID } from "crypto";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import s3 from "../config/s3.js";
-
-// export const uploadManyFiless = async (files, folder) => {
-
-//     try {
-//         if (!files || Object.keys(files).length === 0) {
-//             throw new Error("No files received for uploading ")
-//         }
-
-//         const uploadPromises = Object.entries(files).map(
-//             async ([fieldName, fileArray]) => {
-//                 // console.log("fileArray", fieldName, fileArray);
-//                 const result = await cloudinary.uploader.upload(
-//                     fileArray[0].path, {
-//                     folder: `Nexaro/${folder}`,
-//                     resource_type: "auto",
-//                 })
-//                 return {
-//                     fieldName,
-//                     url: result.secure_url,
-//                     format: result.format,
-//                     public_id: result.public_id
-//                 }
-//             })
-
-//         const uploadResult = await Promise.all(uploadPromises);
-
-//         const result = {};
-
-//         uploadResult.forEach((file) => {
-//             result[file.fieldName] = {
-//                 url: file.url,
-//                 format: file.format,
-//                 public_id: file.public_id
-//             };
-//         });
-//         console.log("-------------------------------------------------");
-//         console.log("res", result)
-//         console.log("-------------------------------------------------");
-
-//         return result;
-
-//     } catch (error) {
-//         console.log("Error in uploading files : ", error)
-//         return { error }
-//     }
-// }
 
 export const uploadManyFiles = async (files, folder) => {
     try {
